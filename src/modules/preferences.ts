e35f9de91774263a23b61c3d78db257e6f49e767
exp <114413207+everydayisbetty@users.noreply.github.com>
2026-05-20 01:13:15 +0100

    Fix preferences pane registration

--- src/modules/preferences.ts
+++ src/modules/preferences.ts
@@ -2,6 +2,7 @@ export { initPreferencePane };
 
 function initPreferencePane() {
   Zotero.PreferencePanes.register({
+    id: `${addon.data.config.addonRef}-preferences`,
     src: rootURI + "chrome/content/preferences.xhtml",
     pluginID: addon.data.config.addonID,
     label: "Bionic",
